a66d0c8ea134d2219cb58c463236add732f2f174
DabAZ <dabaz@dabaz.me>
2024-09-17 04:11:49 +0800

    add i18n support

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,17 +2,19 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>DabNotion</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/dabaz-official/dabnotion',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'dabaz-official/dabnotion',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'DabNotion by DabAZ',
   },
 }
+
+i18n: [
+  { locale: 'en-US', text: 'English' },
+  { locale: 'zh-CN', text: '中文' },
+]
 
 export default config
